76a134e4ccd2ee7ac9179318da9f223db7748354
komimsp <99100783+komimsp@users.noreply.github.com>
2025-05-05 10:14:06 +0200

    Update __version__.py

--- msp_tls_client/__version__.py
+++ msp_tls_client/__version__.py
@@ -4,8 +4,8 @@
 #  / / / /____\ \_____/ /___| | |  __/ | | | |_
 #  \/  \____/\__/     \____/|_|_|\___|_| |_|\__|
 
-__title__ = "msp_tls_client"
+__title__ = "MSP-TLS-Client"
 __description__ = "Advanced Python HTTP Client."
 __version__ = "0.1.0"
-__author__ = "Xerus"
+__author__ = "komimsp"
 __license__ = "MIT"
